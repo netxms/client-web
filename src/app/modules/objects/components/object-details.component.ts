750206e180ea2a26cfac52bdcdab692888c7f246
Victor Kirhenshtein <victor@netxms.org>
2019-01-10 14:38:42 +0200

    object header layout in object details view

--- src/app/modules/objects/components/object-details.component.ts
+++ src/app/modules/objects/components/object-details.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NetObj, ObjectsService } from '@app/services/objects.service';
 
 @Component({
-  selector: 'app-object-details',
-  templateUrl: './object-details.component.html',
-  styleUrls: ['./object-details.component.css']
+   selector: 'app-object-details',
+   templateUrl: './object-details.component.html',
+   styleUrls: ['./object-details.component.css']
 })
 export class ObjectDetailsComponent implements OnInit {
+   object: NetObj;
 
-  constructor() { }
+   private objectId: number;
+   private subscription: Subscription;
 
-  ngOnInit() {
-  }
+   constructor(private route: ActivatedRoute, private objectService: ObjectsService) { }
 
+   ngOnInit() {
+      this.subscription = this.route.params.subscribe(params => {
+         this.objectId = +params['objectId'];
+         this.object = null;
+         const s = this.objectService.getObject(this.objectId).subscribe(object => {
+            this.object = object;
+            s.unsubscribe();
+         })
+      });
+   }
+
+   ngOnDestroy() {
+      this.subscription.unsubscribe();
+   }
 }
